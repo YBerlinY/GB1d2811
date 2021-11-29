aad6ceb95dccc0bb7b2784a425eac4e82ec5a322
YBerlinY <Berlin.yuriy2013@yandex.ru>
2021-11-29 22:07:56 +0300

    Initial commit

---
+++ src/lesson1/l1/Main.java
@@ -0,0 +1,31 @@
+package lesson1.l1;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("hi");
+//        System.out.println("hi");
+//        System.out.println("hi");
+//
+//        int a = 5;
+//        int b = 6;
+//        int c = a + b;
+//        System.out.println(a %= 2.5);
+//
+//        System.out.println(c);
+//        System.out.printf("%d+%d=%d", a, b, c);
+//
+//        int aa = 6;
+//        double bb = 9;
+//        aa = (int) bb;
+        Main maint = new Main();
+        for (int i = 0; i < 5; i++) {
+            maint.print();
+        }
+    }
+
+    void print() {
+        System.out.println("===========");
+        System.out.println("== hello ==");
+        System.out.println("===========");
+    }
+}
